64a6a9499c8ef7ac84cefdb1158cea56c219694f
khadije <khadijeh.allahgholi@gmail.com>
2022-05-11 18:47:24 +0430

    Q2 changed

--- Qs-JS/app2.js
+++ Qs-JS/app2.js
@@ -1,18 +1,25 @@
 function recurIndex(str){
     let arr = [];
     let arr2 = [];
+    let obj = {};
     let str2 = str.split('');
     console.log(str2);
     for (let i=0; i<str2.length; i++) {
-        for (var j=i+1; j<str2.length; j++) {
+        for (let j=i+1; j<str2.length; j++) {
             if (str[i] === str[j]) {
                 arr.push(str[i]);
-                arr2.push(i);
+                //arr2.push(i);
             }
         }
+
     }
     console.log(arr);
-    console.log(arr2);
+    //console.log(arr2);
+    // for (let i=0; i<arr.length; i++) { 
+    //     if (arr2.indexOf(arr[i]) == -1) {
+    //         arr2.push(arr[i]);
+    //     }
+   
 }   
 recurIndex("AREDCBSDERD");
 
